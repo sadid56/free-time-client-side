cfad11f98d1cbedc1a4c6e2b97cbd37923caec39
sadid56 <sadidhasan56@gmail.com>
2023-12-26 16:00:32 +0600

    update a feed page

--- src/shared/Feed/Feed.jsx
+++ src/shared/Feed/Feed.jsx
@@ -9,13 +9,15 @@ import { useState } from "react";
 // import FeedsShareModal from "../../Components/FeedsShareModal/FeedsShareModal";
 import toast from "react-hot-toast";
 import { IoIosShareAlt } from "react-icons/io";
+import { MdClose } from "react-icons/md";
+import { HiDotsVertical } from "react-icons/hi";
 
 const Feed = ({ feed , refetch}) => {
   const { name, article, time, likes, comments, _id, auther_image, image } = feed;
   const axiosPublic = useAxiosPublic()
   const [liked, setLiked] = useState(false)
   const [likeCount, setLikeCount] = useState(likes); 
-
+  const [isToggle, setIsToggle] = useState(false);
   const handleLike = async()=>{
         try{
           await axiosPublic.post(`/feeds/likes/${_id}`)
@@ -41,15 +43,28 @@ const Feed = ({ feed , refetch}) => {
 
   return (
     <div className="p-5 border rounded-md">
-      <div className="flex items-center gap-2">
-        <div className="avatar">
-          <div className="w-10 rounded-full">
-            <img src={auther_image} />
+      <div className="flex justify-between items-center gap-2 relative">
+        <div className="flex items-center gap-2">
+          <div className="avatar">
+            <div className="w-10 rounded-full">
+              <img src={auther_image} />
+            </div>
+          </div>
+          <div>
+            <h3 className="text-xl font-medium">{name}</h3>
+            <p className="text-sm text-gray-500">{time?.slice(0, 10)}</p>
           </div>
         </div>
-        <div>
-          <h3 className="text-xl font-medium">{name}</h3>
-          <p className="text-sm text-gray-500">{time.slice(0,10)}</p>
+        <button onClick={() => setIsToggle(!isToggle)} className={`text-2xl `}>
+          {isToggle ? <MdClose /> : <HiDotsVertical />}
+        </button>
+        <div
+          className={`${
+            isToggle
+              ? "absolute right-4 top-9 bg-slate-700 p-5 rounded-md shadow-md"
+              : "hidden"
+          } flex flex-col gap-2 `}>
+          <button onClick={shareHandler} className="flex items-center gap-1 text-xl text-white hover:bg-slate-600 bg-slate-400 px-4 py-2 rounded-md transform-all duration-300"><IoIosShareAlt /> Share</button>
         </div>
       </div>
       <h5 className="font-medium my-5">{article}</h5>
@@ -59,7 +74,7 @@ const Feed = ({ feed , refetch}) => {
       </div>
 
       {/* react  */}
-      <div className="flex justify-between px-10 my-2 border-2 p-2 rounded-md">
+      <div className="flex justify-between mt-5 px-16 border-2 p-2 rounded-md">
         {
           liked ? <button className="flex items-center text-pink-500 gap-1 text-xl">
           <BiSolidLike /> {likeCount}
@@ -69,8 +84,7 @@ const Feed = ({ feed , refetch}) => {
       </button>
         }
          <CommentsModal comments={comments} id={_id} refetch={refetch}/>
-        {/* <FeedsShareModal/> */}
-        <button onClick={shareHandler} className="flex items-center gap-1 text-xl"><IoIosShareAlt /> Share</button>
+        
       </div>
     </div>
   );
