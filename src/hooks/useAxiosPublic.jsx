78cc3b9b5a429b76696a4444632146a9f451ad18
sadid56 <sadidhasan56@gmail.com>
2024-03-29 12:46:01 +0600

    add a feeds search functionality

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic  = axios.create({
-    baseURL: 'http://localhost:9000/'
+    baseURL: 'https://free-time-server-side.vercel.app/'
 })
 //http://localhost:9000/
 const useAxiosPublic = () => {
